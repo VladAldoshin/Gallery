a4f6bb944481b31aacaf81a2a1f52d9b68e80381
VladAldoshin <aldoshinvladislav87@gmail.com>
2025-08-20 18:58:08 +0300

    js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.clear()
+const stage = document.querySelector('svg')
+const imgFg = document.querySelector('#imgFg')
+const imgBg = document.querySelector('#imgBg')
+const imgs = [
+  'https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=2000&q=50',
+  'https://images.unsplash.com/photo-1511576661531-b34d7da5d0bb?w=2000&q=50',
+  'https://images.unsplash.com/photo-1476610182048-b716b8518aae?w=2000&q=50',
+  'https://images.unsplash.com/photo-1502657877623-f66bf489d236?w=2000&q=50',
+  'https://images.unsplash.com/photo-1506361797048-46a149213205?w=2000&q=50'
+]
