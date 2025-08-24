71ff3dfea0b22e3bb52ddf15426c695b8274620a
VladAldoshin <aldoshinvladislav87@gmail.com>
2025-08-24 10:05:11 +0300

    add js code

--- js/index.js
+++ js/index.js
@@ -9,3 +9,17 @@ const imgs = [
   'https://images.unsplash.com/photo-1502657877623-f66bf489d236?w=2000&q=50',
   'https://images.unsplash.com/photo-1506361797048-46a149213205?w=2000&q=50'
 ]
+
+const pos = {x:innerWidth/2, y:innerHeight/2}
+
+for (let i=0; i<imgs.length; i++){
+  const img = document.createElementNS("http://www.w3.org/2000/svg", "image")
+  imgBg.appendChild(img)
+  gsap.set(img, {attr:{x:'-5%', y:'-5%', width:'110%', height:'110%', href:imgs[i], preserveAspectRatio:'xMidYMid slice'}})
+}
+
+window.addEventListener('resize', ()=>{
+  pos.x = innerWidth/2
+  pos.y = innerHeight/2
+  gsap.set('circle', {duration:0.3, attr:{cx:pos.x, cy:pos.y}})
+})
